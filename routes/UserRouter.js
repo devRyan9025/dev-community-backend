3436dc00ba6484180f9b9fd633920088ddb77328
devRyan9025 <alex7006@naver.com>
2025-04-01 16:06:00 +0900

    Refactor: 전체 유저 조회는 jwt 토큰 없이도 가능하게 리팩토링

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -9,7 +9,7 @@ const { isAuthenticatedJwt } = require('../middlewares/isAuthenticatedJwt');
 const userRouter = express.Router();
 
 // JWT 토큰으로 보호
-userRouter.get('/getAllUsers', isAuthenticatedJwt, getAllUsers);
+userRouter.get('/getAllUsers', getAllUsers);
 
 // ✅ 로그인한 사용자 정보
 userRouter.get('/me', isAuthenticatedJwt, getCurrentUser);
